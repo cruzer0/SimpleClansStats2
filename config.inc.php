ff8458f3ec71ae1c4595dec4db1736a33e7b9dc8
Luca Postiglione <l.postiglione@gmx.at>
2013-09-22 20:09:58 +0200

    Some directory, styling and HTML changes.

---
+++ config.inc.php
@@ -0,0 +1,44 @@
+<?
+
+/* ===================== Database Settings =====================
+ * Database from SimpleClans2 (Same values as in your config.yml)
+ */
+$dbhost = 'mc.realpvp.eu';
+$dbport = 3306;
+$dbuser = 'ni16467_1_DB';
+$dbpass = '18WMpsph';
+$dbname = 'ni16467_1_DB';
+
+/* ===================== Website Settings =====================
+ * $sitelogo <- There you can set your logo. If you haven't one
+ *              just leave it empty.
+ * $sitename <- This only appears if the $sitelogo variable is
+ *              empty.
+ */
+$sitelogo = "";
+$sitename = "SimpleClansStats2";
+
+/* ======================== Kill Weights ========================
+ * Can be found arround line 42 in your config.yml.
+ * Default values already set, no need to change.
+ */
+$civ = 1.0;   // = civilian: 0.5 
+$neu = 1.0;   // = neutral: 1.0
+$riv = 2.0;   // = rival: 2.0
+
+/* ======================= DynMap Settings =======================
+ * Here you can set your DynMap URL.
+ * If you don't need it just leave 'none'.
+ * May be broken, I haven't tested, as I don't use.
+ * Contact me if you have issues: l.postiglione@gmx.at
+ */
+$dynmap = 'none'; //Example: '92.51.171.14:8123'
+
+/* ====== End of Configuration (DO NOT EDIT BELOW THIS LINE) ====== */
+@$db = new mysqli($dbhost, $dbuser, $dbpass, $dbname, $dbport);
+if ($db->connect_errno) {
+    echo $db->connect_error;
+    exit();
+}
+$db->set_charset("UTF-8");
+?>
--- index.php
+++ index.php
@@ -1,3 +1,63 @@
-<?php
-include("SimpleClansStats.php");
+<?
+require_once 'config.inc.php';
 ?>
+<!DOCTYPE html>
+<head>
+    <title><? echo $sitename; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Le CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+
+    <!-- Le JavaScript -->
+    <script src="//code.jquery.com/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/stats.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="navbar navbar-default">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><? echo empty($sitelogo) ? $sitename : '<img src="' . $sitelogo . '" alt="' . $sitename . '">'; ?></a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="<? echo $_SERVER["PHP_SELF"]; ?>?show=home">Home</a></li>
+                    <li><a href="<? echo $_SERVER["PHP_SELF"]; ?>?show=clans">Clans</a></li>
+                    <li><a href=="<? echo $_SERVER["PHP_SELF"]; ?>?show=players">Players</a></li>
+                    <li><a href=="<? echo $_SERVER["PHP_SELF"]; ?>?show=Info">Info</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+        <?php
+        switch ($_GET['content']) {
+            case "showInfo":
+                include("showInfo.php");
+                break;
+            case "showPlayers":
+                include("showPlayers.php");
+                break;
+            case "showClans":
+                include("showClans.php");
+                break;
+            case "showHome":
+                include("showDefault.php");
+                break;
+            default:
+                include("showDefault.php");
+                break;
+        }
+        ?>
+    </div> <!-- /container -->
+    <a href='http://dev.bukkit.org/server-mods/simpleclansstats' target='_blank' style='font-size:0.8em;color:black;text-decoration:none;'>SimpleClansStats</a></td>
+</body>
+</html>
+
+
